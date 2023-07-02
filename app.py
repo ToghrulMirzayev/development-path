ab5bae5cfde23ed67b061a9a40d6a6fb8ecdb4b3
ToghrulMirzayev <togrul.mirzoev@gmail.com>
2023-07-02 22:41:01 +0200

    added config.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
 from teams import manage_teams_bp
 from developer import manage_skills_bp
+import os
 
 app = Flask(__name__)
+env_config = os.getenv("PROD_APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
+
 
 @app.route('/')
 def buttons():
     return render_template('index.html')
+
 
 app.register_blueprint(manage_teams_bp)
 app.register_blueprint(manage_skills_bp)
